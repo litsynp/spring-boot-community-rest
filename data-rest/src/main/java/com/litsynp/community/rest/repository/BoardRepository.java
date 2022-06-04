66735d9f4a11c6780293b448eaacf4f950537806
litsynp <nocte55is@gmail.com>
2022-06-04 22:09:03 +0900

    feat: support filtering

--- data-rest/src/main/java/com/litsynp/community/rest/repository/BoardRepository.java
+++ data-rest/src/main/java/com/litsynp/community/rest/repository/BoardRepository.java
@@ -2,8 +2,11 @@ package com.litsynp.community.rest.repository;
 
 import com.litsynp.community.rest.domain.Board;
 import com.litsynp.community.rest.domain.projection.BoardOnlyContainTitle;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource(excerptProjection = BoardOnlyContainTitle.class)
@@ -12,4 +15,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     @Override
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     <S extends Board> S save(S entity);
+
+    @RestResource(path = "query")
+    List<Board> findByTitle(@Param("title") String title);
 }
